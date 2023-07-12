56716cd83097eb00440734b51433f799f45d7b15
Beneduelle <vik9703@gmail.com>
2023-07-12 21:18:27 +0300

    Created UserRepository, UserService, UserServiceImpl

---
+++ src/main/java/com/example/SpringJWT/service/impl/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.SpringJWT.service.impl;
+
+import com.example.SpringJWT.domain.User;
+import com.example.SpringJWT.domain.UserPrincipal;
+import com.example.SpringJWT.repository.UserRepository;
+import com.example.SpringJWT.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+@Transactional
+@Qualifier("UserDetailsService")
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private UserRepository userRepository;
+    private Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username);
+        if (user == null) {
+            LOGGER.error("User not found by username: " + username);
+            throw new UsernameNotFoundException("User not found by username: " + username);
+        } else {
+            user.setLastLoginDateDisplay(user.getLastLoginDate());
+            user.setLastLoginDate(new Date());
+            userRepository.save(user);
+            UserPrincipal userPrincipal = new UserPrincipal(user);
+            LOGGER.info("Returning found user by username: " + username);
+            return userPrincipal;
+        }
+    }
+}
