989c2db72f56423356b2941d7f2f48b0b0e3cc5a
Beneduelle <vik9703@gmail.com>
2023-07-12 20:49:17 +0300

    Created JwtAuthenticationEntryPoint

---
+++ src/main/java/com/example/SpringJWT/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,43 @@
+package com.example.SpringJWT.filter;
+
+import com.example.SpringJWT.domain.HttpResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static com.example.SpringJWT.constant.SecurityConstant.FORBIDDEN_MESSAGE;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    /**
+     * Overrided method sends better response(with description) to user
+     * @param request
+     * @param response
+     * @param exception
+     * @throws IOException
+     */
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException exception)
+            throws IOException {
+
+        HttpResponse httpResponse = new HttpResponse(FORBIDDEN.value(),
+                FORBIDDEN,
+                FORBIDDEN.getReasonPhrase().toUpperCase(),
+                FORBIDDEN_MESSAGE);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
